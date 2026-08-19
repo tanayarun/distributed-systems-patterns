a4cb9168beba03f837f3837fd2b400b515a4357d
tanayarun <ankulwartanay@gmail.com>
2026-08-19 15:58:45 +0530

    added server and logging

---
+++ 01-sidecar/webapp/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+type statusRecorder struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+func (rec *statusRecorder) WriteHeader(code int) {
+	rec.statusCode = code
+	rec.ResponseWriter.WriteHeader(code)
+}
+
+func sampleHandler(w http.ResponseWriter, r *http.Request) {
+	rec := &statusRecorder{
+		ResponseWriter: w, statusCode: 200,
+	}
+
+	f, err := os.OpenFile("../shared/webapp.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0o664)
+	if err != nil {
+		fmt.Println(err)
+		http.Error(rec, "internal error", http.StatusInternalServerError)
+		return
+	}
+
+	defer func() {
+		_ = f.Close()
+	}()
+
+	defer func() {
+		logline := fmt.Sprintf(" statuscode: %v, time: %v, method: %v, path: %v\n", rec.statusCode, time.Now(), r.Method, r.URL.Path)
+
+		_, err := f.WriteString(logline)
+		if err != nil {
+			fmt.Println("Could not write to file: ", err)
+		}
+	}()
+
+	_, err = rec.Write([]byte("logged\n"))
+	if err != nil {
+		fmt.Println("could no write to client: ", err)
+	}
+}
+
+func main() {
+	http.HandleFunc("/", sampleHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
